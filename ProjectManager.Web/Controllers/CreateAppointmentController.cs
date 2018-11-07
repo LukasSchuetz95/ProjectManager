a9a4ef81a258fbe8d3c3d7ea75c864f22cfd7604
MairingerManuel <44291815+MairingerManuel@users.noreply.github.com>
2018-11-07 16:21:15 +0100

    uow

--- ProjectManager.Web/Controllers/CreateAppointmentController.cs
+++ ProjectManager.Web/Controllers/CreateAppointmentController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Core.Contracts;
+using ProjectManager.Core.Entities;
+using ProjectManager.Web.Models;
 
 namespace ProjectManager.Web.Controllers
 {
     public class CreateAppointmentController : Controller
     {
+
+        IUnitOfWork _unitOfWork;
+
+        public CreateAppointmentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult CreateAppointment()
         {
             return View();
