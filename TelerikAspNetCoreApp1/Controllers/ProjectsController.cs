0eab51860a3a42eb6bfb78f1b4e1e4b70ed1e074
LukasSchuetz <42680907+LukasSchuetz95@users.noreply.github.com>
2019-01-16 15:48:22 +0100

    commit

---
+++ TelerikAspNetCoreApp1/Controllers/ProjectsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Core.Contracts;
+using TelerikAspNetCoreApp1.Models;
+
+namespace TelerikAspNetCoreApp1.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        IUnitOfWork _unitOfWork;
+
+        public ProjectsController(IUnitOfWork unitofwork)
+        {
+            _unitOfWork = unitofwork;
+        }
+
+        public IActionResult List()
+        {
+            ProjectsListViewModel model = new ProjectsListViewModel();
+            model.LoadData(_unitOfWork);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult List(ProjectsListViewModel model)
+        {
+            model.Projects = _unitOfWork.Projects.GetProjectByName(model.FilterProjectName);
+            return View(model);
+        }
+    }
+
+}
