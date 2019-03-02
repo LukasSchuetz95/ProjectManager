43543c2343997d8f55e1ae6a01ec6f3c56251355
LukasSchuetz <42680907+LukasSchuetz95@users.noreply.github.com>
2019-03-02 02:45:00 +0100

    .

--- ProjectManager.Web/Areas/Identity/Data/ProjectManagerWebUser.cs
+++ ProjectManager.Web/Areas/Identity/Data/ProjectManagerWebUser.cs
@@ -10,9 +10,9 @@ namespace ProjectManager.Web.Areas.Identity.Data
     // Add profile data for application users by adding properties to the ProjectManagerWebUser class
     public class ProjectManagerWebUser : IdentityUser
     {
-        public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
-        public virtual ICollection<IdentityUserLogin<string>> Logins { get; set; }
-        public virtual ICollection<IdentityUserToken<string>> Tokens { get; set; }
-        public virtual ICollection<IdentityUserRole<string>> UserRoles { get; set; }
+        //public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
+        //public virtual ICollection<IdentityUserLogin<string>> Logins { get; set; }
+        //public virtual ICollection<IdentityUserToken<string>> Tokens { get; set; }
+        //public virtual ICollection<IdentityUserRole<string>> UserRoles { get; set; }
     }
 }
