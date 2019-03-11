bfe7e7edc30b2d96fd04c5faf05a767bd2c5ab8e
Lukas Schuetz <lukas.schuetz1@gmail.com>
2019-03-11 19:39:23 +0100

    .

--- ProjectManager.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ProjectManager.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -139,8 +139,6 @@ namespace ProjectManager.Web.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    RedirectToAction("CreateEmployee", "Employees");
                     return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
